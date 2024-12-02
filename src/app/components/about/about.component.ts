382dba991fe449465da12d165f1c6c9b58a990f3
PHatma <faaatmahaaani@gmail.com>
2024-12-02 09:50:09 +0200

    new

---
+++ src/app/components/about/about.component.ts
@@ -0,0 +1,16 @@
+import { Component } from '@angular/core';
+import { StarComponent } from '../star/star.component';
+
+@Component({
+  selector: 'app-about',
+  standalone: true,
+  imports: [StarComponent],
+  templateUrl: './about.component.html',
+  styleUrl: './about.component.css',
+})
+export class AboutComponent {
+  title: string = 'about me';
+  color: string = 'white';
+
+
+}
---
+++ src/app/components/portfolio/portfolio.component.ts
@@ -0,0 +1,20 @@
+import { Component } from '@angular/core';
+import { StarComponent } from '../star/star.component';
+
+@Component({
+  selector: 'app-portfolio',
+  standalone: true,
+  imports: [StarComponent],
+  templateUrl: './portfolio.component.html',
+  styleUrl: './portfolio.component.css',
+})
+export class PortfolioComponent {
+  flag: boolean = true;
+  modelImg: string = '';
+
+
+  hideModel(element: EventTarget | null, img: HTMLImageElement): void {
+    if (element == img) return;
+    this.flag = true;
+  }
+}
